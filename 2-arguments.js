5bbb1dbf31a41f8f51d477687d18cead7c2ef72a
unknown <you@example.com>
2025-06-01 18:30:36 +0100

    spelling error fixed

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,7 +3,7 @@ const Me = process.argv.slice(2);
 if(Me.length === 0){
     console.log('No argument');
 }else if (Me.length === 1){
-     console.log('Arguement found');
+     console.log('Argument found');
 }else {
     console.log('Arguments found');
 }
